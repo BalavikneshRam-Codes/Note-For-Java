22681657ec39680eccb440b387084564c9165965
BalavikneshRam-Codes <balaram0711@gmail.com>
2024-02-18 21:14:09 +0530

    update

---
+++ DSAforjava/InsertionSort.java
@@ -0,0 +1,29 @@
+package DSAforjava;
+
+public class InsertionSort {
+    public static void main(String args[]){
+
+        int[] arr = {3,4,5,1,9,2};
+        int[] num = insertionSort(arr);
+        for(int n:num){
+            System.out.print(n+" ");
+        }
+
+    }
+    public static int[] insertionSort(int[] arr){
+        int size = arr.length;
+
+
+        for(int i = 1;i<size;i++){
+            int key = arr[i];
+
+            int j = i - 1;
+            while (j >= 0 && arr[j] > key) {
+                arr[j+1] = arr[j];
+                j--;
+            }
+            arr[j+1] = key;
+        }
+        return arr;
+    }
+}
