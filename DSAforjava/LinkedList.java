88d7a05d286b9034b4caf87c6606791f7c0bf2f4
BalavikneshRam-Codes <balaram0711@gmail.com>
2024-02-21 21:13:22 +0530

    update

---
+++ DSAforjava/LinkedList.java
@@ -0,0 +1,10 @@
+package DSAforjava;
+
+public class LinkedList {
+    public static void main(String args[]){
+        LLinkedList list = new LLinkedList();
+        list.insert(1);
+        list.insert(2);
+        list.show();
+    }
+}
---
+++ DSAforjava/Node.java
@@ -0,0 +1,6 @@
+package DSAforjava;
+
+public class Node{
+    int data;
+    Node next;
+}
