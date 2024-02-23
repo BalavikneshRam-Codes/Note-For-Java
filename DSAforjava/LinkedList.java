1cc3a0764408645b0f2405a51e67d8773fe4bb4f
BalavikneshRam-Codes <balaram0711@gmail.com>
2024-02-23 13:32:43 +0530

    update

--- DSAforjava/LinkedList.java
+++ DSAforjava/LinkedList.java
@@ -5,6 +5,40 @@ public class LinkedList {
         LLinkedList list = new LLinkedList();
         list.insert(1);
         list.insert(2);
+        list.insert(3);
         list.show();
     }
+    public static class LLinkedList {
+        static Node head;
+        public void insert(int data){
+            Node node = new Node();
+            node.data = data;
+            node.next = null;
+    
+    
+            if(head == null){
+                head = node;
+            }else{
+                Node n = head;
+                while(n.next != null){
+                    n = n.next;
+                }
+                n.next = node;
+            }
+        }
+    
+        public void show(){
+            Node node = head;
+            while(node.next != null){
+                System.out.println(node.data);
+                node = node.next;
+            }
+            System.out.print(node.data);
+        }
+    }
+    public static class Node{
+        int data;
+        Node next;
+    }
+    
 }
--- DSAforjava/Node.java
+++
@@ -1,6 +0,0 @@
-package DSAforjava;
-
-public class Node{
-    int data;
-    Node next;
-}
