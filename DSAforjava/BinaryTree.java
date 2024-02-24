fe21316a8e6f2d18c0a7081b2f041b9c6f35a534
BalavikneshRam-Codes <balaram0711@gmail.com>
2024-02-24 16:33:56 +0530

    update

--- DSAforjava/BinaryTree.java
+++ DSAforjava/BinaryTree.java
@@ -4,9 +4,11 @@ public class BinaryTree {
     bt.insert(8);
     bt.insert(7);
     bt.insert(12);
-    bt.insert(15 );
+    bt.insert(15);
     bt.insert(2);
     bt.insert(5);
+    bt.insert(1);
+
 
     bt.InOrder();
     System.out.println("------------");
